8dd7ee0552e068f3e5cb1b4816627212ef52a191
siyadas878 <siyadas878@gmail.com>
2023-06-23 22:52:52 +0530

    some alignments were chnaged in nowplaying screen

--- lib/screens/nowPlaying.dart
+++ lib/screens/nowPlaying.dart
@@ -41,7 +41,7 @@ class _NowPlayingScreenState extends State<NowPlayingScreen> {
           end: Alignment.bottomLeft,
         ),
       ),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+      child: Column(
         children: [
           Container(
             height: 70,
@@ -118,212 +118,221 @@ class _NowPlayingScreenState extends State<NowPlayingScreen> {
           SizedBox(
             height: 10,
           ),
-          Text(
-            'Now Playing',
-            style: GoogleFonts.oswald(
-                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
-          ),
-          ElevatedButton(
-              style:
-                  ElevatedButton.styleFrom(backgroundColor: Color(0xFFF0ECC2)),
-              onPressed: () async {
-                String lyrics = await fetchlyrics(
-                    player.getCurrentAudioTitle, player.getCurrentAudioArtist);
-                await Future.delayed(Duration(seconds: 3));
-
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return Container(
-                      color: Color.fromARGB(255, 26, 35, 24),
-                      child: SingleChildScrollView(
-                          child: Padding(
-                        padding:  EdgeInsets.all(20.0),
-                        child: Text(
-                          lyrics,
-                          style: GoogleFonts.oswald(
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFFF0ECC2),
-                              fontSize: 20),
-                        ),
-                      )),
-                    );
-                  },
-                );
-              },
-              child: Text(
-                'Lyrics',
-                style: GoogleFonts.oswald(
-                  fontWeight: FontWeight.w500,
-                ),
-              )),
-
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10),
-            height: 300,
-            width: 250,
-            child: Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25)),
-              color: const Color(0xFFF0ECC2),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(25),
-                        topLeft: Radius.circular(25)),
-                    child: PlayerBuilder.current(
-                      player: player,
-                      builder: (context, playing) {
-                        int id = int.parse(playing.audio.audio.metas.id!);
-                        currentlyplayingfinder(id);
-                        return QueryArtworkWidget(
-                          size: 3000,
-                          quality: 100,
-                          keepOldArtwork: true,
-                          artworkQuality: FilterQuality.high,
-                          artworkBorder: BorderRadius.circular(0),
-                          artworkFit: BoxFit.cover,
-                          artworkWidth: double.infinity,
-                          artworkHeight: 210,
-                          id: int.parse(playing.audio.audio.metas.id!),
-                          type: ArtworkType.AUDIO,
-                          nullArtworkWidget: ClipRRect(
-                            borderRadius: BorderRadius.circular(0),
-                            child: Image.asset(
-                              'assets/images/lead.jpeg',
-                              width: double.infinity,
-                              height: 210,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 20, left: 20),
-                      child: PlayerBuilder.current(
-                        player: player,
-                        builder: (context, playing) {
-                          return Column(
-                            children: [
-                              Text(
-                                player.getCurrentAudioTitle,
-                                overflow: TextOverflow.ellipsis,
-                                style: GoogleFonts.oswald(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              SizedBox(
-                                height: 3,
-                              ),
-                              Text(player.getCurrentAudioArtist,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: GoogleFonts.oswald(
-                                      fontWeight: FontWeight.w300,
-                                      color: Colors.black)),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20),
-              child:
-                  player.builderRealtimePlayingInfos(builder: (context, Infos) {
-                Duration currentposition = Infos.currentPosition;
-                Duration totalduration = Infos.duration;
-
-                return ProgressBar(
-                  progress: currentposition,
-                  total: totalduration,
-                  progressBarColor: Color(0xFF000000),
-                  thumbColor: Color(0xFFF0ECC2),
-                  onSeek: (duration) {
-                    player.seek(duration);
-                  },
-                );
-              })),
-          SizedBox(height: 30),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      shuffleMode = !shuffleMode;
-                    });
-                    player.toggleShuffle();
-                  },
-                  icon: Icon(Icons.shuffle,
-                      color: shuffleMode ? Color(0xFFF0ECC2) : Colors.black)),
-              SizedBox(width: 20),
-              CircleAvatar(
-                radius: 20,
-                backgroundColor: Color(0xFF000000),
-                child: IconButton(
-                    onPressed: () async {
-                      if (nextprevdone) {
-                        nextprevdone = false;
-                        await player.previous();
-                        nextprevdone = true;
-                      }
-                    },
-                    icon: Icon(
-                      Icons.skip_previous,
-                      color: Color(0xFFF0ECC2),
-                    )),
+              Text(
+                'Now Playing',
+                style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 20),
               ),
-              SizedBox(width: 20),
-              PlayPauseButton(),
-              SizedBox(width: 20),
-              CircleAvatar(
-                radius: 20,
-                backgroundColor: Color(0xFF000000),
-                child: IconButton(
-                    onPressed: () async {
-                      if (nextprevdone) {
-                        nextprevdone = false;
-                        await player.next();
-                        nextprevdone = true;
-                      }
-                    },
-                    icon: Icon(
-                      Icons.skip_next,
-                      color: Color(0xFFF0ECC2),
-                    )),
-              ),
-              SizedBox(width: 20),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      repeatMode = !repeatMode;
-                    });
-                    player.setLoopMode(LoopMode.single);
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Color(0xFFF0ECC2)),
+                  onPressed: () async {
+
+                    String lyrics = await fetchlyrics(
+                        player.getCurrentAudioTitle, player.getCurrentAudioArtist);
+                    await Future.delayed(Duration(seconds: 3));
+
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          color: Color.fromARGB(255, 26, 35, 24),
+                          child: SingleChildScrollView(
+                              child: Padding(
+                            padding:  EdgeInsets.all(20.0),
+                            child: Text(
+                              lyrics,
+                              style: GoogleFonts.oswald(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFF0ECC2),
+                                  fontSize: 20),
+                            ),
+                          )),
+                        );
+                      },
+                    );
                   },
-                  icon: Icon(
-                    Icons.repeat,
-                    color: repeatMode ? Color(0xFFF0ECC2) : Colors.black,
-                  ))
+                  child: Text(
+                    'Lyrics',
+                    style: GoogleFonts.oswald(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )),
+
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                height: 300,
+                width: 250,
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                  color: const Color(0xFFF0ECC2),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(25),
+                            topLeft: Radius.circular(25)),
+                        child: PlayerBuilder.current(
+                          player: player,
+                          builder: (context, playing) {
+                            int id = int.parse(playing.audio.audio.metas.id!);
+                            currentlyplayingfinder(id);
+                            return QueryArtworkWidget(
+                              size: 3000,
+                              quality: 100,
+                              keepOldArtwork: true,
+                              artworkQuality: FilterQuality.high,
+                              artworkBorder: BorderRadius.circular(0),
+                              artworkFit: BoxFit.cover,
+                              artworkWidth: double.infinity,
+                              artworkHeight: 210,
+                              id: int.parse(playing.audio.audio.metas.id!),
+                              type: ArtworkType.AUDIO,
+                              nullArtworkWidget: ClipRRect(
+                                borderRadius: BorderRadius.circular(0),
+                                child: Image.asset(
+                                  'assets/images/lead.jpeg',
+                                  width: double.infinity,
+                                  height: 210,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: PlayerBuilder.current(
+                            player: player,
+                            builder: (context, playing) {
+                              return Column(
+                                children: [
+                                  Text(
+                                    player.getCurrentAudioTitle,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: GoogleFonts.oswald(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                  SizedBox(
+                                    height: 3,
+                                  ),
+                                  Text(player.getCurrentAudioArtist,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: GoogleFonts.oswald(
+                                          fontWeight: FontWeight.w300,
+                                          color: Colors.black)),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: player.builderRealtimePlayingInfos(
+                      builder: (context, Infos) {
+                    Duration currentposition = Infos.currentPosition;
+                    Duration totalduration = Infos.duration;
+
+                    return ProgressBar(
+                      progress: currentposition,
+                      total: totalduration,
+                      progressBarColor: Color(0xFF000000),
+                      thumbColor: Color(0xFFF0ECC2),
+                      onSeek: (duration) {
+                        player.seek(duration);
+                      },
+                    );
+                  })),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          shuffleMode = !shuffleMode;
+                        });
+                        player.toggleShuffle();
+                      },
+                      icon: Icon(Icons.shuffle,
+                          color:
+                              shuffleMode ? Color(0xFFF0ECC2) : Colors.black)),
+                  SizedBox(width: 20),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Color(0xFF000000),
+                    child: IconButton(
+                        onPressed: () async {
+                          if (nextprevdone) {
+                            nextprevdone = false;
+                            await player.previous();
+                            nextprevdone = true;
+                          }
+                        },
+                        icon: Icon(
+                          Icons.skip_previous,
+                          color: Color(0xFFF0ECC2),
+                        )),
+                  ),
+                  SizedBox(width: 20),
+                  PlayPauseButton(),
+                  SizedBox(width: 20),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Color(0xFF000000),
+                    child: IconButton(
+                        onPressed: () async {
+                          if (nextprevdone) {
+                            nextprevdone = false;
+                            await player.next();
+                            nextprevdone = true;
+                          }
+                        },
+                        icon: Icon(
+                          Icons.skip_next,
+                          color: Color(0xFFF0ECC2),
+                        )),
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          repeatMode = !repeatMode;
+                        });
+                        player.setLoopMode(LoopMode.single);
+                      },
+                      icon: Icon(
+                        Icons.repeat,
+                        color: repeatMode ? Color(0xFFF0ECC2) : Colors.black,
+                      ))
+                ],
+              )
             ],
           )
         ],
@@ -344,4 +353,3 @@ Widget CreaateAppBar() {
     ],
   );
 }
-
